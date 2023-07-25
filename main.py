fe0735f74eb2abb07b56bf1434d9942ffa59c953
Oli Legg <oli.legg@silverchip.com>
2023-07-25 22:36:49 +0100

    Progress so far

--- main.py
+++ main.py
@@ -4,50 +4,99 @@ from dotenv import load_dotenv
 import pandas as pd
 import requests
 
-comment_line = '\n___________\n'
-header_line =  '\n###########\n'
+COMMENT_LINE = "\n___________\n"
+HEADER_LINE = "\n###########\n"
+
 
 def load_credentials():
     load_dotenv()
     credentials = {
         "server": os.environ.get("SERVER"),
         "username": os.environ.get("USERNAME"),
-        "password": os.environ.get("PASSWORD"), # Probably your account api key - not ur password 
+        "password": os.environ.get(
+            "PASSWORD"
+        ),  # Probably your account api key - not ur password
         "project_key": os.environ.get("PROJECT_KEY"),
     }
     return credentials
 
+
+credentials = load_credentials()
+
+
+jira_http_get = lambda url, stream=False: requests.get(
+    url,
+    stream=stream,
+    auth=(credentials["username"], credentials["password"]),
+)
+
+
 def get_issues(credentials, start_at=0, max_results=100):
-    response = requests.get(
-        f"{credentials['server']}/rest/api/2/search?jql=project={credentials['project_key']}&fields=comment,summary,description,status,created&maxResults={max_results}&startAt={start_at}",
-        auth=(credentials['username'], credentials['password'])
+    response = jira_http_get(
+        f"{credentials['server']}/rest/api/2/search?jql=project={credentials['project_key']}&fields=comment,summary,description,status,created,attachment&maxResults={max_results}&startAt={start_at}"
     )
     response.raise_for_status()
     data = response.json()
     return data["issues"]
 
+
 def get_public_comments_from_issue(issue):
     public_comments = []
     for comment in issue["fields"]["comment"]["comments"]:
-        if comment['jsdPublic']:
-            author_name = comment['author']['displayName']
-            creation_date = comment['created']
-            comment_body = comment['body']
-            comment_info = f"{header_line}Author: {author_name}\nDate: {creation_date}{header_line}Comment: {comment_body}"
+        if "jsdPublic" in comment and comment["jsdPublic"]:
+            author_name = comment["author"]["displayName"]
+            creation_date = comment["created"]
+            comment_body = comment["body"]
+            comment_info = f"{HEADER_LINE}Author: {author_name}\nDate: {creation_date}{HEADER_LINE}Comment: {comment_body}"
             public_comments.append(comment_info)
-    return comment_line.join(public_comments)
+    return COMMENT_LINE.join(public_comments)
+
+
+def make_dir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+
+def download_attachments(issue_key, attachments):
+    make_dir("attachments")
+    for attachment in attachments:
+        response = jira_http_get(attachment["content"], stream=True)
+        try:
+            response.raise_for_status()
+            make_dir(os.path.join("attachments", issue_key))
+            with open(
+                os.path.join(
+                    "attachments",
+                    issue_key,
+                    attachment["filename"],
+                ),
+                "wb",
+            ) as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+        except Exception as e:
+            with open(os.path.join("attachments", "failed-downloads.txt"), "a") as file:
+                file.write(f"Failed {issue_key} {attachment['filename']}: {e}\n")
+
 
 def get_data_from_issue(issue):
-    ticket = issue['key']
-    summary = issue['fields']['summary']
-    description = issue['fields']['description']
-    status = issue['fields']['status']['name']
-    created = issue['fields']['created']
+    ticket = issue["key"]
+    summary = issue["fields"]["summary"]
+    description = issue["fields"]["description"]
+    status = issue["fields"]["status"]["name"]
+    created = issue["fields"]["created"]
     public_comments = get_public_comments_from_issue(issue)
-    return {"Ticket": ticket, "Summary": summary, "Description": description, "Status": status, "Created": created, "Public Comments": public_comments}
+    return {
+        "Ticket": ticket,
+        "Summary": summary,
+        "Description": description,
+        "Status": status,
+        "Created": created,
+        "Public Comments": public_comments,
+    }
+
 
 def main():
-    credentials = load_credentials()
     df = pd.DataFrame(columns=["Ticket", "Public Comments"])
     start_at = 0
     while True:
@@ -55,10 +104,15 @@ def main():
         if not issues:
             break
         for issue in issues:
-            data = get_data_from_issue(issue)
+            download_attachments(issue["key"], issue["fields"]["attachment"])
+            data = get_data_from_issue  (issue)
             df = pd.concat([df, pd.DataFrame([data])], ignore_index=True)
         start_at += 100
-    df.to_csv(f"{credentials['project_key']}-Export-{datetime.today().strftime('%Y-%m-%d')}.csv", index=False)
+    df.to_csv(
+        f"{credentials['project_key']}-Export-{datetime.today().strftime('%Y-%m-%d')}.csv",
+        index=False,
+    )
+
 
 if __name__ == "__main__":
     main()
